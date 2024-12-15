eae37c3106eba278bd06fd736c241c379b754a41
thangsaberhero <thangsaberhero@gmail.com>
2024-12-15 22:18:45 +0700

    Add files via upload

---
+++ utils/setupDB.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+
+# Function to run .sql file
+def executeScriptsFromFile(filename):
+    # Open and read the file as a single buffer
+    fd = open(filename, 'r')
+    sqlFile = fd.read()
+    fd.close()
+
+    # All SQL commands (split on ';')
+    sqlCommands = sqlFile.split(';')
+    mydb = mysql.connector.connect(
+        host="34.126.94.224",
+        port=3306,
+        user="root",  # change as required
+        passwd="123456",  # change as required
+    )
+    c = mydb.cursor()
+
+    # Execute every command from the input file
+    for command in sqlCommands:
+        # This will skip and report errors
+        try:
+            c.execute(command)
+        except (mysql.connector.Error) as e:
+            print(e)
+    mydb.commit()
+    mydb.close()
+
+
+# Setup main database
+executeScriptsFromFile('LibraryDB.sql')
+# Setup database for unit testing
+#executeScriptsFromFile('LibraryTestDB.sql')
